bf46a7d1c4444ecbc332e4fc263fcf030c8828ca
Premysl 'Anydot' Hruby <dfenze@gmail.com>
2011-03-01 17:30:54 +0100

    jopt.pl: use DB_File insted of xattr, optimize in parallel

--- jopt.pl
+++ jopt.pl
@@ -3,88 +3,97 @@ use warnings;
 use strict;
 
 use File::Find;
-use File::ExtAttr qw/getfattr setfattr/;
+use Cwd qw/fast_abs_path/;
+use DB_File;
+use IPC::Run qw/start/;
+
+die "Sorry, HOME env not found :-/"
+	unless $ENV{HOME};
+
+my $db_t = tie my %db, 'DB_File', $ENV{HOME}.'/.jopt.db'
+	or die "Can't open db: $!";
 
 my $jpegre = qr/\.jpe?g$/i;
 
 my $gain = 0;
+my $num = 0;
 
 sub cb {
 	my $file = $_;
+	my $realfile = fast_abs_path $file;
 
 	return
-		unless -f $file;
+		unless -f $realfile;
 	return
-		unless $file =~ /$jpegre/;
+		unless $realfile =~ /$jpegre/;
 
 	
-	print "$File::Find::name ";
+	print "$file ";
 
-	my $mtime = (stat($file))[9];
-	my $eatime = getfattr($file, "jopt");
+	my $mtime = (stat($realfile))[9];
+	my $eatime = $db{$realfile};
 
 	if (defined $eatime && $mtime == $eatime) {
 		print "-\n";
 		return;
 	}
 
-	if (-e "$file.jopt.$$.o" or -e "$file.jopt.$$.p") {
-		warn "Oops, tempfiles already exists ($File::Find::name)";
+	if (-e "$realfile.jopt.$$.o" or -e "$realfile.jopt.$$.p") {
+		warn "Oops, tempfiles already exists ($realfile)";
 		return;
 	}
 
-	if (system("jpegtran -copy no -optimize -outfile $file.jopt.$$.o $file") >> 8) {
-		warn "Error while running jpegtran";
+	my $normal = start [qw/jpegtran -copy no -optimize -outfile/, "$realfile.jopt.$$.o", $realfile];
+	my $progres = start [qw/jpegtran -copy no -optimize -progressive -outfile/, "$realfile.jopt.$$.p", $realfile];
+
+	if (!$normal->finish || !$progres->finish) {
+		warn "Error while running jpegtran: $!";
 		return;
 	}
 
-	print ".";
-
-	if (system("jpegtran -copy no -optimize -progressive -outfile $file.jopt.$$.p $file") >> 8) {
-		warn "Error while running jpegtran";
-		unlink "$file.jopt.$$.o";
-		return;
-	}
-
-	print ".";
-
-	my $nsize = (stat($file))[7];
-	my $osize = (stat("$file.jopt.$$.o"))[7];
-	my $psize = (stat("$file.jopt.$$.p"))[7];
+	my $nsize = (stat($realfile))[7];
+	my $osize = (stat("$realfile.jopt.$$.o"))[7];
+	my $psize = (stat("$realfile.jopt.$$.p"))[7];
 
 	if ($nsize <= $osize and $nsize <= $psize) {
 		print "n\n";
-		unlink "$file.jopt.$$.p", "$file.jopt.$$.o";
+		unlink "$realfile.jopt.$$.p", "$realfile.jopt.$$.o";
 	}
 	elsif ($osize <= $psize) {
 		print "o\n";
 		
-		rename "$file.jopt.$$.o", $file
-			or die $@;
-		unlink "$file.jopt.$$.p";
+		rename "$realfile.jopt.$$.o", $realfile
+			or die $!;
+		unlink "$realfile.jopt.$$.p";
 
 		$gain += $nsize - $osize;
 	}
 	else {
 		print "p\n";
 
-		rename "$file.jopt.$$.p", $file
-			or die $@;
-		unlink "$file.jopt.$$.o";
+		rename "$realfile.jopt.$$.p", $realfile
+			or die $!;
+		unlink "$realfile.jopt.$$.o";
 
 		$gain += $nsize - $psize;
 	}
 	
-	$mtime = (stat($file))[9];
-	setfattr($file, "jopt", $mtime);
+	$mtime = (stat($realfile))[9];
+	$db{$realfile} = $mtime;
+
+	$db_t->sync
+		unless $num++ % 20;
 }
 
-if (@ARGV) {
-	find(\&cb, @ARGV);
-}
-else {
-	find(\&cb, ".");
-}
+@ARGV = "."
+	unless @ARGV;
+
+find({
+	wanted => \&cb,
+	no_chdir => 1,
+	},
+	@ARGV,
+);
 
 print "\n\nGain after optimization is $gain Bytes\n"; 
 
